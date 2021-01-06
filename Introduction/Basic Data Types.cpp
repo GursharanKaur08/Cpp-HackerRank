f817ae7b2b59fa30bfccc75a4640a21361f2139a
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-01-06 21:59:21 +0530

    Create Basic Data Types.cpp

---
+++ Introduction/Basic Data Types.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    char ch;
+    float c;
+    double d;
+    scanf("%d %ld %c %f %lf",&a,&b,&ch,&c,&d);
+    printf("%d\n%ld\n%c\n%f\n%lf\n",a,b,ch,c,d);
+    // Complete the code.
+    return 0;
+}
