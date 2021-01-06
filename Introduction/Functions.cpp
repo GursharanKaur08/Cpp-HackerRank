f50a6c47b88259c37831434b8e86531519cd4eee
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-01-06 22:13:15 +0530

    Update Functions.cpp

--- Introduction/Functions.cpp
+++ Introduction/Functions.cpp
@@ -3,12 +3,14 @@
 using namespace std;
 
 int max_of_four(int a, int b, int c, int d)
-{   int max;
+{   
+    int max;
     max = (a > b ? a : b);
     max = (c > d? (c>max? c:max) : (d>max? d:max));
     return max;
 }
-int main() {
+int main() 
+{
     int a, b, c, d;
     scanf("%d %d %d %d", &a, &b, &c, &d);
     int ans = max_of_four(a, b, c, d);
