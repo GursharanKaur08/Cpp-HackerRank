7e0ba04edb4b99dd83efcb6a86bacc285d3a47b0
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-01-14 23:20:00 +0530

    Update Virtual Functions.cpp

--- Classes/Virtual Functions.cpp
+++ Classes/Virtual Functions.cpp
@@ -10,26 +10,32 @@ class Person
     public:
     string name;
     int age;
-    virtual void getdata() {
+    virtual void getdata() 
+    {
         cin >> this->name >> this->age;
     }
-    virtual void putdata() {
+    virtual void putdata() 
+    {
         cout << this->name << " " << this->age << endl;
     }
 };
 
-class Professor : public Person {
-public:
-    Professor() {
+class Professor : public Person 
+{
+    public:
+    Professor() 
+    {
         this->cur_id = ++id;
     }
     int publications;
     static int id;
     int cur_id;
-    void getdata() {
+    void getdata() 
+    {
         cin >> this->name >> this->age >> this->publications;
     }
-    void putdata() {
+    void putdata() 
+    {
         cout << this->name << " "
             << this->age << " "
             << this->publications << " "
@@ -38,24 +44,30 @@ public:
 };
 int Professor::id = 0;
 
-class Student : public Person {
-#define NUM_OF_MARKS 6
-public:
-    Student() {
+class Student : public Person 
+{
+    #define NUM_OF_MARKS 6
+    public:
+    Student() 
+    {
         this->cur_id = ++id;
     }
     int marks[NUM_OF_MARKS];
     static int id;
     int cur_id;
-    void getdata() {
+    void getdata() 
+    {
         cin >> this->name >> this->age;
-        for (int i=0; i<NUM_OF_MARKS; i++) {
+        for (int i=0; i<NUM_OF_MARKS; i++) 
+        {
             cin >> marks[i];
         }
     }
-    void putdata() {
+    void putdata() 
+    {
         int marksSum = 0;
-        for (int i=0; i<NUM_OF_MARKS; i++) {
+        for (int i=0; i<NUM_OF_MARKS; i++) 
+        {
             marksSum += marks[i];
         }
         cout << this->name << " "
@@ -66,24 +78,22 @@ public:
 };
 int Student::id = 0;
 
-int main(){
-
+int main()
+{
     int n, val;
-    cin>>n; //The number of objects that is going to be created.
+    cin>>n;
     Person *per[n];
 
-    for(int i = 0;i < n;i++){
-
+    for(int i = 0;i < n;i++)
+    {
         cin>>val;
-        if(val == 1){
-            // If val is 1 current object is of type Professor
+        if(val == 1)
+        {
             per[i] = new Professor;
-
         }
-        else per[i] = new Student; // Else the current object is of type Student
+        else per[i] = new Student;
 
-        per[i]->getdata(); // Get the data from the user.
-
+        per[i]->getdata(); 
     }
 
     for(int i=0;i<n;i++)
